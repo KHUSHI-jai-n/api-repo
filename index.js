f792f5ffdb156dd0df325ad879c53cac8448572d
KHUSHI-jai-n <khushijain.mp@gmail.com>
2024-11-21 17:35:18 +0530

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const mime = require('mime');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
